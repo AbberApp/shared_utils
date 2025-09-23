6c187a82898b2670210012d3c3124e7bf00728c6
Zinon-Software <business.zinon@gmail.com>
2025-09-23 18:59:00 +0300

    enbulde shared utils package

---
+++ lib/shared_utils.dart
@@ -0,0 +1,14 @@
+library shared_utils;
+
+export 'file_manager/file_picker_manager.dart';
+export 'file_manager/image_picker_manager.dart';
+
+export 'network/connection_status.dart';
+
+export 'utils/functions.dart';
+export 'utils/permission_handler.dart';
+export 'utils/intl_phone_field_utils.dart';
+export 'utils/delay_handler.dart';
+export 'utils/app_constants.dart';
+
+export 'widgets/page_indicator_dot_widget.dart';
